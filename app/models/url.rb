3f8a07653fa3b747d97b0bbf4204cd8031a544d0
Mohan Ramchandani <Ramchandani@Mohans-MacBook-Pro.local>
2017-07-05 11:48:19 +0800

    added create and private method

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,16 @@
 class Url < ApplicationRecord
+  validates :long_url, length: { minimum: 15 }
+
+  def shorten
+    self.short_url = SecureRandom.hex(5)
+  end
+
+  def add_http
+    if self.long_url.start_with?("http://") == true
+      self.long_url = self.long_url
+    else
+      self.long_url.insert(0, "http://")
+    end
+  end
+
 end
